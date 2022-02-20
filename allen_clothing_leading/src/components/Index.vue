f3b37de86badccdff55b9fa0c84871c3446734b2
chenjie <965475253@qq.com>
2022-02-20 23:27:26 +0800

    vue-cli创建

---
+++ allen_clothing_leading/src/components/Index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="Index">
+        {{msg}}
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Index',
+  data () {
+    return {
+      msg: 'hello allen！'
+    }
+  }
+}
+</script>
